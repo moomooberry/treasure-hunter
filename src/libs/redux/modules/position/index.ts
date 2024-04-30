9e3c621a0e90a9f46fcd028cad0eb202b5fe94cb
ghh237 <ghh237@naver.com>
2024-04-30 18:36:23 +0900

    Feat: Types(Position)

--- src/libs/redux/modules/position/index.ts
+++ src/libs/redux/modules/position/index.ts
@@ -1,10 +1,11 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { Position } from "@src/types/position";
 
 const LAT_DEFAULT = 37.5642135;
 const LNG_DEFAULT = 127.0016985;
 
 export interface ReduxPosition {
-  position: { lat: number; lng: number };
+  position: Position;
 }
 
 const initialState: ReduxPosition = {
---
+++ src/types/position/index.ts
@@ -0,0 +1,1 @@
+export type Position = { lat: number; lng: number };
