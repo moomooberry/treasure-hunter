b04c2f86731580871187cd9452a6b88b13fdd789
ghh237 <ghh237@naver.com>
2024-05-28 15:21:59 +0900

    Fix: Page(Treasure_detail) header scroll opacity

--- src/screens/treasure/detail/_layout/header/index.tsx
+++ src/screens/treasure/detail/_layout/header/index.tsx
@@ -1,36 +1,91 @@
 "use client";
 
-import { FC, useEffect, useState } from "react";
-import LayoutHeader from "@src/components/layout/LayoutHeader";
+import { FC, useCallback } from "react";
 import TreasureDetailHeaderOption from "./TreasureDetailHeaderOption";
+import { useScroll, useTransform } from "framer-motion";
+import { motion } from "framer-motion";
+import { useRouter } from "next/navigation";
+import useReduxSelector from "@src/hooks/redux/useReduxSelector";
+import CaretIcon from "@src/components/icons/CaretIcon";
+import { LAYOUT_HEADER_HEIGHT } from "@src/constants/layout";
+
+import STYLE from "./treasure.detail.header.module.scss";
+
+const SCROLL_THRESHOLD = 300;
 
 const TreasureDetailHeader: FC = () => {
-  const [isOverThreshold, setIsOverThreshold] = useState(false);
+  const { back } = useRouter();
 
-  useEffect(() => {
-    const THRESHOLD_SCROLL_VALUE = 300;
+  const paddingTop = useReduxSelector((state) => state.reduxDevice.device.top);
 
-    const onScroll = () => {
-      const scroll = window.scrollY;
-      if (scroll > THRESHOLD_SCROLL_VALUE) {
-        setIsOverThreshold(true);
-      } else {
-        setIsOverThreshold(false);
-      }
-    };
+  const { scrollY } = useScroll();
 
-    window.addEventListener("scroll", onScroll);
+  const opacity = useTransform(scrollY, [0, SCROLL_THRESHOLD], [0, 1]);
 
-    return () => window.removeEventListener("scroll", onScroll);
-  }, []);
+  const opacityReverse = useTransform(scrollY, [0, SCROLL_THRESHOLD], [1, 0]);
+
+  const scale = useTransform(scrollY, [0, SCROLL_THRESHOLD], [0.95, 1]);
+
+  const boxShadow = useTransform(
+    scrollY,
+    [0, SCROLL_THRESHOLD],
+    ["0px 4px 10px rgba(0,0,0,0)", "0px 4px 10px rgba(0,0,0,0.05)"]
+  );
+
+  const onHeaderBackClick = useCallback(() => {
+    back();
+  }, [back]);
 
   return (
-    <LayoutHeader
-      title={isOverThreshold ? "보물 찾기" : ""}
-      backgroundColor={isOverThreshold ? "#fff" : "transparent"}
-      shadowDisabled={!isOverThreshold}
-      option={<TreasureDetailHeaderOption />}
-    />
+    <header
+      className={STYLE.__layout_header_container}
+      style={{
+        paddingTop,
+        height: LAYOUT_HEADER_HEIGHT,
+      }}
+    >
+      <button
+        className={STYLE.__layout_header_container_back_button}
+        onClick={onHeaderBackClick}
+      >
+        <CaretIcon width="14px" height="14px" />
+      </button>
+
+      {/* opacity 0 ~ 1 */}
+      <motion.div
+        className={STYLE.__layout_header_opacity_box}
+        style={{
+          opacity,
+          boxShadow,
+          paddingTop,
+          height: LAYOUT_HEADER_HEIGHT,
+        }}
+      >
+        <motion.b
+          className={STYLE.__layout_header_title}
+          style={{
+            scale,
+          }}
+        >
+          보물 찾기
+        </motion.b>
+      </motion.div>
+
+      {/* opacity 1 ~ 0 */}
+      <motion.div
+        className={STYLE.__layout_header_opacity_reverse_box}
+        style={{
+          opacity: opacityReverse,
+          paddingTop,
+          height: LAYOUT_HEADER_HEIGHT,
+        }}
+      >
+        <div className={STYLE.__layout_header_opacity_reverse_button} />
+        <div className={STYLE.__layout_header_opacity_reverse_button} />
+      </motion.div>
+
+      <TreasureDetailHeaderOption />
+    </header>
   );
 };
 
