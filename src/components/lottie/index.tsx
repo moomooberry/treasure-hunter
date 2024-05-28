6d9889b11347199c0d7bff7401a61a08f4866432
ghh237 <ghh237@naver.com>
2024-05-28 12:29:20 +0900

    Feat: Component(lottie + HeartIcon) + Assets(loading.json)

---
+++ src/components/lottie/index.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { FC, memo, useEffect, useId, useRef } from "react";
+import lottie, { AnimationConfigWithData } from "lottie-web";
+
+interface LottieProps
+  extends Omit<
+    AnimationConfigWithData,
+    "renderer" | "container" | "path" | "name"
+  > {
+  width?: string;
+  height?: string;
+}
+
+const Lottie: FC<LottieProps> = ({
+  width,
+  height,
+  loop = true,
+  autoplay = true,
+  ...rest
+}) => {
+  const lottieRef = useRef<HTMLDivElement>(null);
+
+  const id = useId();
+
+  useEffect(() => {
+    if (!lottieRef.current) return;
+    lottie.loadAnimation({
+      name: id,
+      container: lottieRef.current,
+      renderer: "svg",
+      autoplay,
+      loop,
+      ...rest,
+    });
+
+    return () => {
+      lottie.destroy(id);
+    };
+  }, [autoplay, id, loop, rest]);
+
+  return (
+    <div
+      style={{
+        width,
+        height,
+      }}
+      ref={lottieRef}
+    />
+  );
+};
+
+export default memo(Lottie);
