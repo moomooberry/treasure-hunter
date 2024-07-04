cba46c4940e9a9c7f0a390f610b0516403ff4004
ghh237 <ghh237@naver.com>
2024-07-04 16:00:25 +0900

    Refactor: Page(map_main) Optimization
    Using Dynamic
    result: First Load JS (306kb -> 113kb)

--- src/screens/map/main/MapMainView.tsx
+++ src/screens/map/main/MapMainView.tsx
@@ -5,8 +5,11 @@ import { FC } from "react";
 import LayoutBody from "@src/components/layout/body";
 import LayoutHeader from "@src/components/layout/header";
 import LayoutFooter from "@src/components/layout/footer";
+import dynamic from "next/dynamic";
 
-import MapMainMap from "./_components/map";
+const MapMainMap = dynamic(() => import("./_components/map/index"), {
+  ssr: false,
+});
 
 export interface MapMainViewProps {}
 
