2438ffd34e52b0b8d5bcea7bb431be6f36fe6944
ghh237 <ghh237@naver.com>
2024-07-24 14:37:44 +0900

    Remove: Screen(treasure_form)

--- src/screens/treasure/form/TreasureFormFallback.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { FC } from "react";
-
-import LayoutBodyCommon from "@src/components/layout/body/LayoutBodyCommon";
-
-const TreasureFormFallback: FC = () => <LayoutBodyCommon />;
-
-export default TreasureFormFallback;
--- src/screens/treasure/form/_components/step/TreasureFormStepPayment.tsx
+++
@@ -1,6 +0,0 @@
-const TreasureFormStepPayment = () => {
-  // TODO -> load payment module
-  return <h1>payment</h1>;
-};
-
-export default TreasureFormStepPayment;
